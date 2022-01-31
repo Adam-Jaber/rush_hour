b85e43ebd180d20dd52550e807d7e791dc895a53
adam-jaber <adam.jaber248@gmail.com>
2022-01-31 22:57:18 +0200

    add signup button to send user to signup page

--- carparkgame/login_screen.py
+++ carparkgame/login_screen.py
@@ -20,6 +20,7 @@ class LoginScreen(tk.Frame):
         tk.Entry(self, textvariable=self.password_var, width=40, show="*").grid(row=1, colum=1, padx=5, pady=5)
 
         tk.Button(self, text="Login", command=self.check_info).grid(row=3, column=0, columnspan=2, pady=10)
+        tk.Button(self, text = "Sign up", command=self.master.signup_screen).grid(row=4, column=0, columnspan=2)
 
     def check_info(self):
         con = pg.connect(database='rush_hour', user='postgres', password='jaber2213')
