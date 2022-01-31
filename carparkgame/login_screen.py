133c6ecc4d7e51db6f087247ea93ac500b341350
adam-jaber <adam.jaber248@gmail.com>
2022-01-31 17:18:10 +0200

    create LoginScreen class and its` setup method

---
+++ carparkgame/login_screen.py
@@ -0,0 +1,21 @@
+import psycopg2 as pg
+import tkinter as tk
+
+
+class LoginScreen(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master, height=600, width=600 * (16/9))
+
+        self.master = master
+        self.username_var = tk.StringVar()
+        self.password_var = tk.StringVar()
+
+        self.set_up()
+
+    def set_up(self):
+        tk.Frame(self, text="Username:").grid(row=0, column=0, padx=5, pady=5)
+        tk.Entry(self, textvariable=self.username_var, width=40).grid(row=0, column=1, padx=5, pady=5)
+        tk.Frame(self, text="Password:").grid(row=1, column=0, padx=5, pady=5)
+        tk.Entry(self, textvariable=self.password_var, width=40, show="*").grid(row=1, colum=1, padx=5, pady=5)
+
+        tk.Button(self, text="Login", command=self.check_info).grid(row=3, column=0, columnspan=2, pady=10)
