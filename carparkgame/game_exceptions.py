4eecfa9e20be09f87d3a512adc131461aa087f9b
adam-jaber <adam.jaber248@gmail.com>
2022-02-01 19:16:25 +0200

    add strings dunder method for game exception

--- carparkgame/game_exceptions.py
+++ carparkgame/game_exceptions.py
@@ -3,48 +3,65 @@ class RushHourException(BaseException):
 
 
 class BoardException(RushHourException):
-    pass
+    def __str__(self):
+        return "Board Exception"
 
 
 class LvlExist(BoardException):
-    pass
+    def __str__(self):
+        return "Level does not exist"
 
 
 class CarException(RushHourException):
-    pass
+    def __str__(self):
+        return "Car exception"
 
 
 class MovementException(CarException):
-    pass
+    def __str__(self):
+        return "Car cannot move like that"
 
 
 class OrientException(MovementException):
-    pass
+    def __str__(self):
+        return "Car only moves within its own axis"
 
 
 class CollisionException(MovementException):
-    pass
+    def __str__(self):
+        return "Cant hit cars (shouldn't *)"
 
 
 class BorderException(MovementException):
-    pass
+    def __str__(self):
+        return "Car moves only within the board"
 
 
 class SignupException(RushHourException):
-    pass
+    def __str__(self):
+        return "Invalid info"
 
 
 class InvalidFName(SignupException):
-    pass
+    def __str__(self):
+        return "Invalid first name"
 
 
 class InvalidLName(SignupException):
-    pass
+    def __str__(self):
+        return "Invalid last name"
 
 
 class UsernameExists(SignupException):
-    pass
+    def __str__(self):
+        return "Username already exists"
 
 
 class InvalidPassword(SignupException):
-    pass
+    def __str__(self):
+        return "Invalid password"
+
+
+class ConfirmError(SignupException):
+    def __str__(self):
+        return "Password does not match confirmation"
