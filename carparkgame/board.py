4b6033cd6f582cca2ba1c548eb6b7c91ad4d6e72
adam-jaber <adam.jaber248@gmail.com>
2022-02-02 17:11:26 +0200

    log beating level through check_win method

--- carparkgame/board.py
+++ carparkgame/board.py
@@ -8,17 +8,19 @@ LEVELS_DATA_COLUMNS = ['level_id', 'level_num', 'red', 'purple', 'pink', 'yellow
 
 
 class Board(tk.Frame):
-    def __init__(self, master, level):
+    def __init__(self, master, level, user_id):
         super().__init__(master, width=420, height=420)
 
         self.square_dict = dict()
         self.level = level
+        self.user_id = user_id
 
         for row in range(6):
             for column in range(6):
                 self.square_dict[(row, column)] = Square(self, row, column)
 
         self.pack_squares()
+        self.set_level()
 
     def set_level(self):
         self._clear_board()
@@ -26,9 +28,10 @@ class Board(tk.Frame):
         self.cars_dict = dict()
 
         con = pg.connect(database='rush_hour', user='postgres', password='jaber2213')
-        cur = con.cursor()
-        cur.execute(f'SELECT * FROM levels WHERE level_num = {self.level}')
-        level_info = cur.fetchone()
+        self.cur = con.cursor()
+        self.cur.execute(f'SELECT * FROM levels WHERE level_num = {self.level}')
+        level_info = self.cur.fetchone()
+        self.level_id = level_info[0]
 
         for car in level_info:
             if isinstance(car, str):
@@ -45,3 +48,10 @@ class Board(tk.Frame):
             self.square_dict[(row_, column_)].grid(row=row_, column=column_)
 
     def check_win(self):
+        if self.square_dict[(2, 5)].color == 'red':
+            self.cur.execute(f"""INSERT INTO user_level_passed
+                                VALUES(
+                                {self.user_id},
+                                {self.level_id}
+                                )""")
+            self.cur.commit()
